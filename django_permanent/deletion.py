016cc686e95177ed5c1932b550115261e9e560fc
meteozond <alex@erix.ru>
2014-01-30 13:31:50 +0400

    + Updated Collector to feet new transactions

--- django_permanent/deletion.py
+++ django_permanent/deletion.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from operator import attrgetter
 
-from django.db.models.deletion import Collector as BaseCollector, force_managed
+from django.db import transaction
+from django.db.models.deletion import Collector as BaseCollector
 from django.db.models import signals, sql
 from django.utils import six
 from django.utils.timezone import now
@@ -11,67 +12,66 @@ from . import PERMANENT_FIELD
 
 class PermanentCollector(BaseCollector):
 
-    @force_managed
     def delete(self):
         """
-            Copy of the BaseCollector.delete with soft delete support for PermanentModel
+            Patched the BaseCollector.delete with soft delete support for PermanentModel
         """
         from .models import PermanentModel
-
         time = now()
 
+        # sort instance collections
         for model, instances in self.data.items():
             self.data[model] = sorted(instances, key=attrgetter("pk"))
 
+        # if possible, bring the models in an order suitable for databases that
+        # don't support transactions or cannot defer constraint checks until the
+        # end of a transaction.
         self.sort()
 
-        for model, obj in self.instances_with_model():
-            if not model._meta.auto_created:
-                signals.pre_delete.send(
-                    sender=model, instance=obj, using=self.using
-                )
+        with transaction.commit_on_success_unless_managed(using=self.using):
+            # send pre_delete signals
+            for model, obj in self.instances_with_model():
+                if not model._meta.auto_created:
+                    signals.pre_delete.send(
+                        sender=model, instance=obj, using=self.using
+                    )
 
-        for qs in self.fast_deletes:
-            if issubclass(qs.model, PermanentModel):  # Update PermanentModel instance
-                qs.using(self.using).update(**{PERMANENT_FIELD: time})
-            else:
-                qs._raw_delete(using=self.using)
+            # fast deletes
+            for qs in self.fast_deletes:
+                if issubclass(qs.model, PermanentModel):  # Update PermanentModel instance
+                    qs = sql.UpdateQuery(qs.model)
+                    qs.update_batch([obj.pk for obj in qs], {PERMANENT_FIELD: time}, self.using)
+                else:
+                    qs._raw_delete(using=self.using)
 
-        for model, instances_for_fieldvalues in six.iteritems(self.field_updates):
-            query = sql.UpdateQuery(model)
-            for (field, value), instances in six.iteritems(instances_for_fieldvalues):
-                if issubclass(model, PermanentModel):
+            # update fields
+            for model, instances_for_fieldvalues in six.iteritems(self.field_updates):
+                query = sql.UpdateQuery(model)
+                for (field, value), instances in six.iteritems(instances_for_fieldvalues):
                     query.update_batch([obj.pk for obj in instances],
                                        {field.name: value}, self.using)
 
-        for instances in six.itervalues(self.data):
-            instances.reverse()
+            # reverse instance collections
+            for instances in six.itervalues(self.data):
+                instances.reverse()
 
-        for model, batches in six.iteritems(self.batches):
-            if issubclass(model, PermanentModel):  # Update PermanentModel instance
-                query = sql.UpdateQuery(model)
-                for field, instances in six.iteritems(batches):
-                    query.update_batch([obj.pk for obj in instances], {PERMANENT_FIELD: time}, self.using)
-            else:
-                query = sql.DeleteQuery(model)
-                for field, instances in six.iteritems(batches):
-                    query.delete_batch([obj.pk for obj in instances], self.using, field)
+            # delete instances
+            for model, instances in six.iteritems(self.data):
+                pk_list = [obj.pk for obj in instances]
+                if issubclass(model, PermanentModel):
+                    query = sql.UpdateQuery(model)
+                    query.update_batch(pk_list, {PERMANENT_FIELD: time}, self.using)
+                else:
+                    query = sql.DeleteQuery(model)
+                    query.delete_batch(pk_list, self.using)
 
-        for model, instances in six.iteritems(self.data):
-            pk_list = [obj.pk for obj in instances]
-            if issubclass(model, PermanentModel):  # Update PermanentModel instance
-                query = sql.UpdateQuery(model)
-                query.update_batch(pk_list, {PERMANENT_FIELD: time}, self.using)
-            else:
-                query = sql.DeleteQuery(model)
-                query.delete_batch(pk_list, self.using)
+                if not model._meta.auto_created:
+                    for obj in instances:
+                        signals.post_delete.send(
+                            sender=model, instance=obj, using=self.using
+                        )
 
-        for model, obj in self.instances_with_model():
-            if not model._meta.auto_created:
-                signals.post_delete.send(
-                    sender=model, instance=obj, using=self.using
-                )
-
+        # update collected instances
         for model, instances_for_fieldvalues in six.iteritems(self.field_updates):
             for (field, value), instances in six.iteritems(instances_for_fieldvalues):
                 for obj in instances:
