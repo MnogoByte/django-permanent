522e1dbf54512ea76b0015457679dc96391faef5
meteozond <alex@erix.ru>
2014-01-31 16:48:12 +0400

    + test runner

---
+++ runtests.py
@@ -0,0 +1,34 @@
+import os, sys
+from django.conf import settings
+import django
+
+
+DEFAULT_SETTINGS = dict(
+    INSTALLED_APPS=(
+        'django_permanent',
+        'django_permanent.tests',
+    ),
+    DATABASES={
+        'default':{
+            'ENGINE': 'django.db.backends.sqlite3'
+        }
+    }
+)
+
+
+def runtests(*test_args):
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    if hasattr(django, 'setup'):
+        django.setup()
+    if not test_args:
+        test_args = ['django_permanent']
+
+    parent = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, parent)
+    from django.test.simple import DjangoTestSuiteRunner
+    failures = DjangoTestSuiteRunner(verbosity=1, interactive=True, failfast=False).run_tests(test_args)
+    sys.exit(failures)
+
+if __name__ == '__main__':
+    runtests()
