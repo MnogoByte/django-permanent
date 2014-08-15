a09fd7f91ad291ad294e3b80076e439e3572871d
David Fischer <david.fischer.ch@gmail.com>
2014-08-15 19:52:12 +0400

    Replace commit_on_success_unless_managed by atomic (cherry picked from commit ac5de57)

--- django_permanent/deletion.py
+++ django_permanent/deletion.py
@@ -26,7 +26,7 @@ def delete(self):
     # end of a transaction.
     self.sort()
 
-    with transaction.commit_on_success_unless_managed(using=self.using):
+    with transaction.atomic(using=self.using, savepoint=False):
         # send pre_delete signals
         for model, obj in self.instances_with_model():
             if not model._meta.auto_created:
