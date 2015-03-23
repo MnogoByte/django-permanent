9fef7e51cee1ca1b7e33d6fb4b7e9d9714a94979
meteozond <alex@erix.ru>
2015-03-23 12:21:51 +0400

    * bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='django-permanent',
-      version='1.0.4',
+      version='1.0.5',
       description='Yet another approach to provide soft (logical) delete or masking (thrashing) django models instead of deleting them physically from db.',
       author='Alexander Klimenko',
       author_email='alex@erix.ru',
