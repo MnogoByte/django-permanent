b321fcaceaa70ecf25ded0997f862eb9fdbd07b2
william chu <william.chu@uptickhq.com>
2023-02-14 22:26:33 +1100

    feat: Add mypy configuration

--- django_permanent/settings.py
+++ django_permanent/settings.py
@@ -1,13 +1,16 @@
+import django_stubs_ext
 from django.conf import settings
 
+django_stubs_ext.monkeypatch()
 
-FIELD_CLASS = getattr(settings, 'PERMANENT_FIELD_CLASS', 'django.db.models.DateTimeField')
-FIELD = getattr(settings, 'PERMANENT_FIELD', 'removed')
-FIELD_KWARGS = getattr(settings, 'PERMANENT_FIELD_KWARGS', dict(
-    default=None,
-    null=True,
-    blank=True,
-    editable=False
-))
+FIELD_CLASS = getattr(
+    settings, "PERMANENT_FIELD_CLASS", "django.db.models.DateTimeField"
+)
+FIELD = getattr(settings, "PERMANENT_FIELD", "removed")
+FIELD_KWARGS = getattr(
+    settings,
+    "PERMANENT_FIELD_KWARGS",
+    dict(default=None, null=True, blank=True, editable=False),
+)
 
-FIELD_DEFAULT = FIELD_KWARGS['default']
+FIELD_DEFAULT = FIELD_KWARGS["default"]
